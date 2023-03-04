21f1ba8473893f5b7818d80379e8b0e93354820f
David <dcmejiaz@geosat.com.co>
2023-03-04 09:14:06 -0500

    se crea perfil

--- lib/profile_trips.dart
+++ lib/profile_trips.dart
@@ -5,9 +5,49 @@ class ProfileTrips extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.indigo,
-      child: const Text("Profile"),
+    return Scaffold(
+      body: Stack(
+        children: [
+          ListView(
+            children: [
+              const UserAccountsDrawerHeader(
+                accountName: Text('John Doe'),
+                accountEmail: Text('email@example.com'),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      'https://www.w3schools.com/howto/img_avatar.png'),
+                ),
+              ),
+              Card(
+                child: Column(
+                  children: [
+                    const ListTile(
+                      leading: Icon(Icons.photo_album),
+                      title: Text('Album'),
+                      subtitle: Text('Description'),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                          child: const Text('CANCEL'),
+                          onPressed: () {},
+                        ),
+                        const SizedBox(width: 8),
+                        TextButton(
+                          child: const Text('OK'),
+                          onPressed: () {},
+                        ),
+                        const SizedBox(width: 8),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
